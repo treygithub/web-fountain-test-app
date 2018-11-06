311d35d734e32a339c2c832f7b7120c2313b4e26
robert hunnicutt <robhunn74@gmail.com>
2018-11-06 12:52:33 -0600

    init commit

---
+++ src/app/header.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+const styles = {
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#1f1f1f'
+  },
+  title: {
+    flex: 1,
+    fontSize: '1.5rem',
+    margin: '1rem'
+  },
+  date: {
+    flex: 1,
+    textAlign: 'right',
+    margin: '1rem',
+    color: 'white'
+  }
+};
+
+export class Header extends Component {
+  render() {
+    return (
+      <header style={styles.header}>
+        <p style={styles.title}>
+          <a href="https://github.com/FountainJS/generator-fountain-webapp" target="_blank" rel="noopener noreferrer">
+            Fountain Generator
+          </a>
+        </p>
+        <p style={styles.date}>
+          Generated with FountainJS v1.0.0 on Tue Nov 06 2018 12:38:39 GMT-0600 (Central Standard Time)
+        </p>
+      </header>
+    );
+  }
+}
